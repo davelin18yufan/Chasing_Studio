a0a03cad22d632cd1481dddb12d850689802c1b6
davelin18yufan <davelin30630@gmail.com>
2024-02-09 11:48:43 +0800

    [fix] fix esLint issues

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,12 @@
-import { getPhotosCached, getPhotosCountCached } from "@/cache"
-import { generateOgImageMetaForPhotos } from "@/photo"
-import { Metadata } from "next"
-import Link from "next/link"
+// import { getPhotosCached, getPhotosCountCached } from "@/cache"
+// import { generateOgImageMetaForPhotos } from "@/photo"
+// import { Metadata } from "next"
+// import Link from "next/link"
 import Image from "next/image"
 // import { Label } from "@/components/ui/label"
 // import { Input } from "@/components/ui/input"
 // import { Textarea } from "@/components/ui/textarea"
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 
 export const runtime = "edge"
 
@@ -44,15 +44,21 @@ export default function HomePage() {
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
               Overview
             </h2>
-            <p className="max-w-3xl mx-auto text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              我們是Chasing
-              Studio，致力於以創意和精準捕捉生活中最珍貴的時刻。我們相信每張照片都蘊含著獨特的故事，專業服務涵蓋肖像、活動、婚禮和商業項目等廣泛範疇。
-              在 Chasing
-              Studio，我們深知創造永恆記憶的重要性，這些記憶將被世代傳承。我們的團隊由經驗豐富的攝影師組成，將技術專業與藝術視野相結合，提供令人驚嘆的影像作品，超出客戶的期望。
-              我們堅持追求卓越和客戶滿意度，為每位客戶提供無縫和愉快的體驗。從初次諮詢到最終交付影像，我們始終重視清晰溝通、細心呵護和個性化服務。
-              無論您是慶祝特殊的里程碑、推出新產品還是保存珍貴的回憶，Chasing
-              Studio都會幫助您實現您的願景。立即與我們聯繫，討論您的攝影需求，讓我們捕捉您生活中最重要的時刻。
-            </p>
+            <div className="max-w-3xl mx-auto text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              <p>
+                我們是Chasing
+                Studio，致力於以創意和精準捕捉生活中最珍貴的時刻。我們相信每張照片都蘊含著獨特的故事，專業服務涵蓋肖像、活動、婚禮和商業項目等廣泛範疇。
+              </p>
+              <p>
+                在 Chasing
+                Studio，我們深知創造永恆記憶的重要性，這些記憶將被世代傳承。我們的團隊由經驗豐富的攝影師組成，將技術專業與藝術視野相結合，提供令人驚嘆的影像作品，超出客戶的期望。
+              </p>
+              <p>
+                我們堅持追求卓越和客戶滿意度，為每位客戶提供無縫和愉快的體驗。從初次諮詢到最終交付影像，我們始終重視清晰溝通、細心呵護和個性化服務。
+                無論您是慶祝特殊的里程碑、推出新產品還是保存珍貴的回憶，Chasing
+                Studio都會幫助您實現您的願景。立即與我們聯繫，討論您的攝影需求，讓我們捕捉您生活中最重要的時刻。
+              </p>
+            </div>
           </div>
         </div>
       </div>
